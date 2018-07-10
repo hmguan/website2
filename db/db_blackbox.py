b7fed67cb4944311bfcff4a7f147da7e67ff870d
stefanie <892468286@qq.com>
2018-07-10 14:27:24 +0800

    黑盒子数据库增删改查接口

---
+++ db/db_blackbox.py
@@ -0,0 +1,42 @@
+
+from db import session_obj,user_info,blackbox_temps
+from sqlalchemy import or_
+from sqlalchemy import asc
+from sqlalchemy import desc
+from configuration import config
+import os
+
+
+class blackbox_manager():
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def insert_temps(user_id,temps_types,others,time):
+        tmp = session_obj.query(user_info).filter_by(id=user_id).first()
+        if(tmp ==None):
+            return -1
+        
+        temps_obj = blackbox_temps(user_id = user_id,temps_types= temps_types,others=others,time=time)
+        session_obj.add(temps_obj)
+        session_obj.commit()
+        return temps_obj.id
+
+
+    @staticmethod
+    def temps(user_id):
+        return session_obj.query(blackbox_temps).filter_by(user_id=user_id).order_by(desc(blackbox_temps.time)).all()
+    
+
+    @staticmethod
+    def remove_temps(temps_id):
+        ret = session_obj.query(blackbox_temps).filter_by(id=temps_id).first()
+        if not ret:
+            return -1
+
+        tmp  = session_obj.query(blackbox_temps).get(ret.id)
+
+        session_obj.delete(tmp)
+        session_obj.commit()
+        return 0
+    
