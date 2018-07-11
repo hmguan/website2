9933df43c923f9fbe8f6e5da165b07883a19f499
YeomanXu <jsycxuyueming@163.com>
2018-07-11 21:06:27 +0800

    更改socket推送模式，使用socket自带线程进行推送消息

--- app/soketio/sockio_api.py
+++ app/soketio/sockio_api.py
@@ -5,25 +5,51 @@ import json
 import errtypes
 from pynsp.logger import *
 
+from threading import RLock
+
+thread = None
+thread_lock = RLock()
+
+post_msg_list=[]
+post_room_list=[]
+
 def response_to_client_data(data):
     print('------------------send message to clien:',data)
-    local_socketio.emit('server_response',data,
-                        namespace='/notify_call')
+    post_msg_list.append(data)
 
 def send_messge_to_room(data,room_identify):
-    local_socketio.emit('room_response',{'data': data},namespace='/notify_call',
-                        room_identify=room_identify,broadcast=True)
+    # local_socketio.emit('room_response',{'data': data},namespace='/notify_call',
+    #                     room_identify=room_identify,broadcast=True)
+    msg_dict=dict()
+    msg_dict[room_identify] = data
+    post_room_list.append(msg_dict)
 
-def close_room(room_identify):
-    local_socketio.close_room(room_identify,namespace='/notify_call')
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    while True:
+        local_socketio.sleep(1)
+        while len(post_msg_list) != 0:
+            obj = post_msg_list.pop(0)
+            print('----------obj:',obj)
+            local_socketio.emit('server_response',obj,
+                                namespace='/notify_call')
 
-def broadcast_message(message):
-    local_socketio.emit('server_response',{'data':message},broadcast=True)
+        while len(post_room_list) != 0:
+            obj = post_room_list.pop(0)
+            for key,item in obj.items():
+                local_socketio.emit('room_response',{'data': item},namespace='/notify_call',
+                                    room_identify=key,broadcast=True)
 
 @local_socketio.on('connect', namespace='/notify_call')
 def socketio_connect():
     print('----------------socketio connected------------------')
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = local_socketio.start_background_task(target=background_thread)
     response_to_client_data({'msg_type':errtypes.TypeShell_SokcetIOConnect,'data':'connect success'})
+
+    print('--------------connected successfully---------------')
 
 @local_socketio.on('disconnect', namespace='/notify_call')
 def socketio_disconnect():
