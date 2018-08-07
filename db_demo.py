a3114b26aa6732f7ed3990f16c01346aa574310e
stefanie <892468286@qq.com>
2018-08-07 14:44:10 +0800

    修改xml信息python3  脚本

--- db_demo.py
+++ db_demo.py
@@ -1,11 +1,144 @@
+from thrift.transport import TSocket
+from thrift.protocol import TBinaryProtocol,TCompactProtocol
+from thrift.transport import TTransport
+from hbase.Hbase import *
 
-from hdfs import Client
-from datetime import datetime
-from mrjob.job import MRJob
+import struct
 
+import ptvsd
+ptvsd.settrace(None, ('0.0.0.0', 12345))
+#ptvsd.wait_for_attach()
+
+def encode(n):
+   return struct.pack("i", n)
+
+# Method for decoding ints with Thrift's string encoding
+def decode(s):
+   return int(s) if s.isdigit() else struct.unpack('i', s)[0]
+
+
+   
+class HBaseApi(object):
+    def __init__(self,table='student',host='192.168.0.100',port=9090):
+        self.table = table
+        self.host = host
+        self.port = port
+
+        # Connect to HBase Thrift server
+        self.transport = TSocket.TSocket(self.host, self.port)
+        self.trans = TTransport.TBufferedTransport(self.transport)
+        
+        self.protocol = TBinaryProtocol.TBinaryProtocol(self.trans)
+
+        # Create and open the client connection
+        self.client = Client(self.protocol)
+        self.transport.open()
+
+        # set type and field of column families
+        self.set_column_families([bytes],['info'])
+        self._build_column_families()
+
+
+    def set_column_families(self,type_list,col_list=['info']):
+        self.columnFamiliesType = type_list
+
+        self.columnFamilies = col_list
+
+
+    def _build_column_families(self):
+        tables = self.client.getTableNames()
+        if self.table not in tables:
+            self.__create_table(self.table)
+
+    def __create_table(self,table):
+        columnFamilies = []
+        for columnFamily in self.columnFamilies:
+            name = ColumnDescriptor(name = columnFamily)
+            columnFamilies.append(name)
+        
+        print(type(table),type(columnFamilies))
+
+        self.client.createTable(table,columnFamilies)
+
+    def __del__(self):
+        self.transport.close()
+
+    def __del_table(self,table):
+        self.client.disableTable(table)
+        self.client.deleteTable(table)
+
+    def getColumnDescriptors(self):
+        return self.client.getColumnDescriptors(self.table)
+
+    def put(self, rowKey, qualifier, value):
+        mutations = []
+        rowKey = rowKey.encode('utf-8')
+        if isinstance(value, str):
+            m_name = Mutation(column=(self.columnFamilies[0]+':'+qualifier).encode('utf8'), value=value.encode('utf8'))
+        elif isinstance(value, int):
+            m_name = Mutation(column=(self.columnFamilies[0]+':'+qualifier).encode('utf8'), value=encode(value))
+        mutations.append(m_name)
+        self.client.mutateRow(self.table, rowKey, mutations)
+
+    def puts(self,rowKeys,qualifier,values):
+        mutationsBatch = []
+        if not isinstance(rowKeys,list):
+            rowKeys = [rowKeys] * len(values)
+
+        for i, value in enumerate(values):
+            mutations = []
+            
+            if isinstance(value, str):
+                m_name =Mutation(column=(self.columnFamilies[0]+':'+qualifier), value=value)
+            elif isinstance(value, int):
+                m_name =Mutation(column=(self.columnFamilies[0]+':'+qualifier), value=encode(value))
+            mutations.append(m_name)
+            mutationsBatch.append(BatchMutation(row = rowKeys[i],mutations=mutations))
+        self.client.mutateRows(self.table, mutationsBatch)
+
+    def getRow(self,row, qualifier='name'):
+        row = self.client.getRow(self.table, row)
+        for r in row:
+            rd = {}
+            row = r.row
+            value = r.columns.get('info:'+qualifier).value
+            rd[row] = value
+        
+            return rd
+
+    def getRows(self, rows, qualifier='name'):
+        res = []
+        for r in rows:
+            res.append(self.getRow(r,qualifier))
+        return res
+
+    def scanner(self, numRows=100, startRow='0002', stopRow='0002'):
+        
+        ss = 'info:name'
+        scannerId = self.client.scannerOpenWithStop(self.table,startRow, stopRow,['info:age'])
+        ret = []
+        rowList = self.client.scannerGetList(scannerId, numRows)
+
+        for r in rowList:
+            rd = {}
+            row = r.row
+            value = (r.columns['info:age'].value)
+            rd[row] = value
+            ret.append(rd)
+
+        return ret
 
 def main():
-    pass
+    ha = HBaseApi()
+    ha.put('0002','age','23')
+    rowKeys = [str(key) for key in range(10001,10010)]
+    values = ['fr'+str(val) for val in range(10001,10010)]
+    ha.puts(rowKeys,'name',values)
+    print(ha.scanner())
+    print(ha.getRow('0002','age'))
+    print(ha.getRows(rowKeys))
+
+ 
 
 if __name__ == "__main__":
     main()
---
+++ update_xml.py
@@ -0,0 +1,113 @@
+
+import shutil
+import os
+import xml.etree.ElementTree as ET
+import ptvsd
+
+#ptvsd.settrace(None, ('0.0.0.0', 12345))
+#ptvsd.wait_for_attach()
+
+
+def find_target(root,tar_node):
+    for child in root:
+       if tar_node==child.tag:
+           return child
+    return None
+
+
+def property_mode(index,element,proerty,value):
+    print(element,proerty,value)
+    tree = ET.parse(element[0])
+    root = tree.getroot()
+    
+    # 路径 根节点无需循环
+    loop_num = len(element)-2
+    for i in range(loop_num):
+        root = find_target(root,element[i+2])
+        if None == root:
+            print('index:%d,can not find target node:%s'%(index,element[i+2]))    
+            return
+    
+
+    root.set(proerty, value)
+    tree.write(element[0])
+  
+def element_mode(index,element,value):
+    print(element,value)
+
+    tree = ET.parse(element[0])
+    root = tree.getroot()
+    
+    # 路径 根节点无需循环
+    loop_num = len(element)-2
+    for i in range(loop_num):
+        root = find_target(root,element[i+2])
+        if None == root:
+            print('index:%d,can not find target node:%s'%(index,element[i+2]))    
+            return -1
+    
+    root.text = value
+    tree.write(element[0])
+
+def restore_file(index,path):
+    backup_path = path + '.backup'
+    if False==os.path.exists(backup_path):
+        print('restore file is not exist')
+        return -1
+    
+    if False==os.path.exists(path):
+        print('original file is not exist')
+        return -1
+    
+    #移除原始文件
+    os.remove(path)
+    #还原备份文件
+    os.rename(backup_path,path)
+    return 0
+
+
+def update_file(index,parms):
+    if False==os.path.exists(parms[0]):
+        print('target file is not exist')
+        return 
+    
+    #1.检查还原
+    if 1==len(parms):
+        return restore_file(index,parms[0])
+    
+    #2.备份,覆盖旧备份
+    shutil.copy(parms[0],parms[0]+'.backup')
+
+    #3.修改文件 &代表修改节的属性
+    last_parm = parms[len(parms)-1]
+    pos = last_parm.find('&')
+    if pos<0:
+        element_mode(index,parms[0:len(parms)-1],parms[len(parms)-1])
+    else:
+        property_mode(index,parms[0:len(parms)-1],last_parm[0:pos],last_parm[pos+1:])
+    
+
+    
+def main():
+    if False==os.path.exists('update_xml.txt'):
+        print('update_xml config file is not exist')
+        return
+    
+    file_object = open('update_xml.txt','r')
+    try:
+        index=-1 
+        for line in file_object:
+            index = index+1
+            parms = line.split()
+            if len(parms)<3 and 1!=len(parms):
+                print('index:%d,parms size is less than 3,actual is %d'%(index,len(parms)))
+                continue
+            
+            print('index:%d,'%(index),parms)
+            update_file(index,parms)
+    finally:
+        file_object.close()
+
+
+if __name__ == '__main__':
+    main()
